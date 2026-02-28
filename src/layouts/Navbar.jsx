2b9210e1449e834ca7e47ca4f8bebfaab76230ce
SayemImran <ik262142@gmail.com>
2026-02-28 11:19:16 +0600

    initiated

---
+++ src/layouts/Navbar.jsx
@@ -0,0 +1,19 @@
+const Navbar = () => {
+  return (
+    <>
+    <div>
+        <div>🩸BloodDrops</div>
+        <div>
+            <div>
+                <li>Dashboard</li>
+                <li>Requests</li>
+                <li>Donor</li>
+            </div>
+        </div>
+        <div></div>
+    </div>
+    </>
+  );
+};
+
+export default Navbar;
